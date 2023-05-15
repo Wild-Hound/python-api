126f85d9865c13f102f63eac605b287754953dc5
Wild-Hound <wildhound404@gmail.com>
2023-05-15 22:47:55 +0600

    simple book crud

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import model
+from config import engine
+from router import router
+
+model.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+@app.get("/")
+async def home():
+    return "Hello World"
+
+app.include_router(router, prefix="/book", tags=["book"])
