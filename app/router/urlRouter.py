2f25bcc93ed9bc238c82540a3d8bc167cc5babef
Wild-Hound <wildhound404@gmail.com>
2023-05-20 13:27:51 +0600

    redirect added

--- app/router/urlRouter.py
+++ app/router/urlRouter.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Path
+from fastapi import APIRouter, Response as FastResponse
 from fastapi import Depends
 from config.url_shortenerConfig import SessionLocal
 from sqlalchemy.orm import Session
@@ -33,10 +33,12 @@ async def get_urls(skip: int = 0, limit: int = 100, db: Session = Depends(get_db
 @router.get("/main/{main_url}")
 async def get_main_url(main_url: str, db: Session = Depends(get_db)):
     _url = urlCrud.get_url_by_main(db, main_url)
-    return Response(code="200", message="Success fetch all data", result=_url)
+    return Response(code="307", message="Success fetch all data", result=_url)
 
 
 @router.get("/redirect/{redirect_url}")
-async def get_main_url(redirect_url: str, db: Session = Depends(get_db)):
-    _url = urlCrud.get_url_by_main(db, redirect_url)
+async def get_main_url(redirect_url: str, response: FastResponse, db: Session = Depends(get_db)):
+    _url = urlCrud.get_url_by_redirect(db, redirect_url)
+    response.status_code = 307
+    response.headers['Location'] = _url.main_url
     return Response(code="200", message="Success fetch all data", result=_url)
